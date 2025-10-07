b1b3bb618bc7a5fe843580c6d1aec53b8880758c
Prakarsha <musicstyle1232@gmail.com>
2025-10-07 15:17:00 -0500

    Added courses.py file

---
+++ Backend/API/Routers/courses.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
+from typing import Literal
+
+router = APIRouter()
+
+QUERIES_FILE = "Discord/students.json"
+
+class CourseInput(BaseModel):
+    course_name: str
+    semester: Literal["Fall", "Spring", "Summer"]
+    documents: list[str]
+    instructor_id: int
+    course_description: str
+    credit_points : int
+
+@router.get("/instructors/{instructor_id}/courses")
+async def get_courses(instructor_id: int):
+    """
+    Retrieve a list of all courses by instructor Id.
+    """
+    try:
+
+        return {"message": "List of items"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+
+@router.post("/courses/")
+async def create_course(course: CourseInput):
+    """
+    Create a new course and link it via instructor Id.
+    """
+    try:
+
+        return {"message": "List of items"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
--- Backend/API/main.py
+++ Backend/API/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from .Routers import questions
+from .Routers import courses
 
 app = FastAPI()
 
 app.include_router(questions.router)
+app.include_router(courses.router)
 
 # cd Backend -> uvicorn API.main:app --reload
 #Find it here: http://127.0.0.1:8000/docs
