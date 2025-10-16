93fb57797415e34a80b2aae0beb69bfa6f3d400c
Jesutofunmi Obimakinde <joo@iastate.edu>
2025-10-16 16:40:11 -0500

    adjust make

--- Backend/API/dependencies.py
+++ Backend/API/dependencies.py
@@ -1,21 +1,23 @@
-"""
-Dependency providers for FastAPI routes.
-
-Defines how services, repositories, and DB connections are created
-and injected into route handlers using FastAPI's Depends().
-"""
-
+import os, platform
+from dotenv import load_dotenv
 from fastapi import Depends
 from API.Repository.sql_repository import SQLRepository
 from API.Repository.postgres_connection_manager import PostgresConnectionManager
 from API.Service.document_service import DocumentService
 
+load_dotenv()
 
 def get_connection_manager() -> PostgresConnectionManager:
     """Create and return a PostgreSQL connection manager."""
-    return PostgresConnectionManager(
-        db_url="postgresql://postgres:postgres@localhost:5432/course_gpt"
-    )
+    db_host, db_port, db_name, db_password = os.environ["DB_HOST"], os.environ["DB_PORT"], os.environ["DB_NAME"], os.environ["DB_PASSWORD"]
+    db_user =os.environ["DB_USER"] if platform.system() == "Windows" else os.getlogin()
+
+    if platform.system() == "Darwin":
+        db_url = f"postgresql://{db_user}@{db_host}:{db_port}/{db_name}"
+    else:
+        db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    return PostgresConnectionManager(db_url=db_url)
 
 
 def get_sql_repository(
