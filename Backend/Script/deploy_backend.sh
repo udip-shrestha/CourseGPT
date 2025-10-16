198a90fb41892ef9006d40849daa428672ed8b2f
Adeife <fadahunsiadeife11@gmail.com>
2025-10-16 11:43:35 -0500

    correction in path

--- Backend/Script/deploy_backend.sh
+++ Backend/Script/deploy_backend.sh
@@ -5,7 +5,7 @@ PERSISTENT_ROOT="/home/gitlab-runner/sdmay26-37"
 BACKEND_DIR="$PERSISTENT_ROOT/Backend"
 API_DIR="$BACKEND_DIR/API"
 API_VENV="$API_DIR/.venv"
-DISCORD_ENV="$BACKEND_DIR/Discord/.env"
+DISCORD_ENV="$BACKEND_DIR/.env"
 BRANCH="${CI_COMMIT_REF_NAME:-main}"
 
 echo "Deploy backend on $(hostname) - root=$PERSISTENT_ROOT branch=$BRANCH"
