6af752c78a4c18eacdfaa5c3724e7ff081c6cdfa
Adomale Kiobel <kennyadam14@gmail.com>
2025-11-04 15:33:01 -0600

    Chagned files location

--- Frontend/src/tests/components/homePageTest.tsx
+++ Frontend/src/tests/components/homePageTest.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 // import { Button } from "./ui/button";
 // import { Input } from "./ui/input";
 // import { Label } from "./ui/label";
