ff37a9e85630791823fdc21a35ec3a1ff6b176bd
Adomale Kiobel <kennyadam14@gmail.com>
2025-11-10 14:08:21 -0600

    Added first cypress e2e case

--- Frontend/cypress.config.ts
+++ Frontend/cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-      baseUrl: 'http://localhost:5173',
-
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypressTests/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypressTests/support/e2e.ts",
   },
 });
--- Frontend/cypressTests/e2e/spec.cy.ts
+++ Frontend/cypressTests/e2e/spec.cy.ts
@@ -1,5 +1,20 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-        expect(true).toBe(true)
+/// <reference types="cypress" />
+
+// Test the homepage
+
+describe('Homepage', () => {
+    it('should display the homepage', () => {
+        cy.visit('http://localhost:5173')
+        cy.get('header').should('exist')
+        cy.get('main').should('exist')
+        cy.get('header').find('h1').first().should('have.text', 'CourseGPT')
+    })
+})
+
+// Test the login page
+describe('Login Page', () => {
+    it('should display the login page', () => {
+        cy.visit('http://localhost:5173/login')
+    //     cy.addListener('email', 'password').type('test@example.com', 'password')
     })
 })
